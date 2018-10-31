73b5e955822b6c9ebbd6db60968aeee97f101e46
Jaelin.Ford <jaelin.ford24@gmail.com>
2018-10-31 10:13:45 -0400

    Add tut_strings.py

--- tut_strings.py
+++ tut_strings.py
@@ -1,2 +1,30 @@
 #Print Welcome Message
 print('Hello World')
+
+#Simple Welcome String
+welcome_message = 'Welcome'
+
+print(welcome_message)
+
+#Use \ to excempt one '
+print('Bobby\'s World')
+
+#Multiple line String
+warning = '''This is a warning!! If you
+do that againg you will be punised!!!
+You won't receive another warning.'''
+
+print(warning)
+
+#Prints length of Message
+print(len(welcome_message))
+
+#Prints index(location) of specific character
+print(welcome_message[0])
+print(welcome_message[6])
+
+#Prints index from start point to end point
+#Blank means Beginning/End
+print('Hello World'[0:5])
+print('Hello World'[:5])
+print('Hello World'[6:])
